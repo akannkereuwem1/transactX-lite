5eb100f4753a39759fb35946d74d0488b77b606b
akannkereuwem1 <akannkereuwem@gmail.com>
2026-04-27 15:51:59 +0100

    fix: replace missing welcome view with JSON response

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app'     => config('app.name'),
+        'status'  => 'running',
+        'version' => 'v1',
+    ]);
 });
